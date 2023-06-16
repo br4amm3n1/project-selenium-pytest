bcca029ff7bf3a260a371a94a3e588eb520725a8
br4amm3n1 <minkhalbert@yandex.ru>
2023-06-17 02:00:41 +0700

    made some changes to start the project

--- conftest.py
+++ conftest.py
@@ -1,9 +1,8 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: en-us, fr or ru")
 
 
